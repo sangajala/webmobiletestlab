00f1e0670b38bccaab611cae0689651c215618d1
bharaj05 <bharaj05@gmail.com>
2014-05-21 09:41:20 +0100

    addprojectadmin page

---
+++ src/main/java/com/web/testlab/AddProjectAdminPage.java
@@ -0,0 +1,40 @@
+package com.web.testlab;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AddProjectAdminPage {
+    WebDriver driver = BrowserFactory.getDriver();
+
+        public void enterProjectAdmins(String fullname,String uname,String pwd,String confpwd,String email,String favplace,String project)
+        {
+            driver.findElement(By.id("fulnme")).sendKeys(fullname);
+            driver.findElement(By.id("usrnme")).sendKeys(uname);
+            driver.findElement(By.id("pass")).sendKeys(pwd);
+            driver.findElement(By.id("cpass")).sendKeys(confpwd);
+            driver.findElement(By.id("email")).sendKeys(email);
+            driver.findElement(By.id("favplace")).sendKeys(favplace);
+            Utils.sleep(5);
+            Utils.selectFromDropDown(By.id("projectid"), project);
+            Utils.sleep(5);
+
+
+        }
+        public void saveProjectAdmins()
+        {
+            driver.findElement(By.xpath(".//*[@id='createprojectadmin']/div/button[2]")).click();
+            Utils.sleep(5);
+
+        }
+
+        public void clikok()
+        {
+            driver.findElement(By.xpath(".//*[@id='success_dialog']/div/div/div/button")).click();
+
+        }
+}
+
+
+
+
